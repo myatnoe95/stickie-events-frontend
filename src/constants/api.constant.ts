191625248b2662f4f62d41c30040980fdc9ae9c9
myatnoe95 <khingsunthwe@gmail.com>
2024-08-04 23:29:39 +0700

    feat: Add API constants

---
+++ src/constants/api.constant.ts
@@ -0,0 +1,4 @@
+export const TOKEN_TYPE = "Bearer ";
+export const REQUEST_HEADER_AUTH_KEY = "Authorization";
+export const API_URL = process.env.NEXT_PUBLIC_API_URL;
+export const API_IMAGE_URL = process.env.NEXT_PUBLIC_API_IMAGE_URL;
