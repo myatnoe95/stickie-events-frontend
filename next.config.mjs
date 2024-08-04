044ce140fc2e17e6a51ee0e4d55d12786d5a416b
myatnoe95 <khingsunthwe@gmail.com>
2024-08-04 23:32:49 +0700

    chore: Update next.config.mjs with image domains and transpile packages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+	reactStrictMode: true,
+	images: {
+		domains: ["localhost", "127.0.0.1"],
+	},
+	transpilePackages: [
+		"antd",
+		"@ant-design",
+		"rc-util",
+		"rc-pagination",
+		"rc-picker",
+		"rc-notification",
+		"rc-tooltip",
+		"rc-tree",
+		"rc-table",
+	],
 };
 
 export default nextConfig;
